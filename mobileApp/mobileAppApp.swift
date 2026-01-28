c7ba47252db080a8c387c5fcb3abd801b28895f8
AHMET190578 <ahmetcanizgi449@gmail.com>
2026-01-28 17:51:38 +0300

    Initial Commit

---
+++ mobileApp/mobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileAppApp.swift
+//  mobileApp
+//
+//  Created by mobie app on 28.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct mobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
